a378c254141361bab46d8bc1813685544d2a42b2
holybaechu <66154521+baechooYT@users.noreply.github.com>
2022-09-03 14:57:30 +0900

    Update verify.js

--- routes/api/verify.js
+++ routes/api/verify.js
@@ -5,8 +5,31 @@ const whois = require('whois-json');
 router.get('/', async function(req, res) {
      // Get the isp by address
      var result = await whois(req.headers['x-forwarded-for'] || req.socket.remoteAddress)
-     console.log(result)
-     res.json(result)
+
+     // Checking is the real roblox server
+     if (
+    result.availableAt == "https://www.arin.net/resources/registry/whois/tou/ https://www.arin.net/resources/registry/whois/tou/" &&
+    result.netRange == '128.116.0.0 - 128.116.127.255' &&
+    result.netType == "Direct Allocation" &&
+    result.orgName == "Roblox" &&
+    result.orgAbuseHandle == 'OPERA503-ARIN' &&
+    result.orgAbuseName == 'OPERATIONS' &&
+    result.orgAbusePhone == '+1-415-655-1982' &&
+    result.orgAbuseEmail == 'noc@roblox.com' &&
+    result.orgAbuseRef == 'https://rdap.arin.net/registry/entity/OPERA503-ARIN'&&
+    result.orgNocHandle == 'OPERA503-ARIN' &&
+    result.orgNocName == 'OPERATIONS' &&
+    result.orgNocPhone == '+1-415-655-1982' &&
+    result.orgNocEmail == 'noc@roblox.com' &&
+    result.orgNocRef == 'https://rdap.arin.net/registry/entity/OPERA503-ARIN'&&
+    result.orgTechHandle == 'OPERA503-ARIN' &&
+    result.orgTechName == 'OPERATIONS' &&
+    result.orgTechPhone == '+1-415-655-1982' &&
+    result.orgTechEmail == 'noc@roblox.com' &&
+    result.orgTechRef == 'https://rdap.arin.net/registry/entity/OPERA503-ARIN'
+    ){
+        console.log("REAL ROBLOX SERVER !@#$!@#!#@!@!");
+    }
 });
 
 module.exports = router;
