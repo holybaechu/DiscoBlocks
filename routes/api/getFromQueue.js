e6fa5c8e0b6a521c9c504520f508c3dcf763da5f
holybaechu <66154521+baechooYT@users.noreply.github.com>
2022-09-04 17:29:55 +0900

    Update getFromQueue.js

--- routes/api/getFromQueue.js
+++ routes/api/getFromQueue.js
@@ -6,7 +6,7 @@ const ipChecker = require('../../functions/checkIsRobloxIP.js');
 router.get('/', async function(req, res) {
     var usernameToGet = req.query.username
     if (!usernameToGet) { res.status(400).json({ success: false, errors: [{message: 'username could not be parsed from requset.'}]}) }
-    if (ipChecker(req.headers['x-forwarded-for'] || req.socket.remoteAddress) != true){res.status(400).json({success: false, errors: [{message: "This ip is not seems to be allowed to access this api."}]}); return;}
+    if (ipChecker(req.headers['x-forwarded-for'] || req.socket.remoteAddress) != true){console.log('error: ' + usernameToGet); res.status(400).json({success: false, errors: [{message: "This ip is not seems to be allowed to access this api."}]}); return;}
 
     if (globalsModule.verifyQueue[usernameToGet]) {
         res.json({ success: true, data: {tag: globalsModule.verifyQueue[usernameToGet].tag} })
