53c44fdfe21141035c187851083f2df4a3886528
holybaechu <66154521+baechooYT@users.noreply.github.com>
2022-09-04 18:17:00 +0900

    Update getFromQueue.js

--- routes/api/getFromQueue.js
+++ routes/api/getFromQueue.js
@@ -5,13 +5,13 @@ const ipChecker = require('../../functions/checkIsRobloxIP.js');
 
 router.get('/', async function(req, res) {
     var usernameToGet = req.query.username
-    if (ipChecker(req.headers['x-forwarded-for'] || req.socket.remoteAddress) != true){console.log('A'); res.status(400).json({success: false, errors: [{message: "This ip is not seems to be allowed to access this api."}]}); return;}
-    if (!usernameToGet) {console.log('A'); res.status(400).json({ success: false, errors: [{message: 'username could not be parsed from requset.'}]}) }
+    if (ipChecker(req.headers['x-forwarded-for'] || req.socket.remoteAddress) != true){console.log('8'); res.status(400).json({success: false, errors: [{message: "This ip is not seems to be allowed to access this api."}]}); return;}
+    if (!usernameToGet) {console.log('9'); res.status(400).json({ success: false, errors: [{message: 'username could not be parsed from requset.'}]}) }
 
     if (globalsModule.verifyQueue[usernameToGet]) {
         res.json({ success: true, data: {tag: globalsModule.verifyQueue[usernameToGet].tag} })
     }else{
-        console.log('A')
+        console.log('14')
         res.status(400).json({ success: false, errors: [{message: 'username is not in the queue.'}]})
     }
 });
