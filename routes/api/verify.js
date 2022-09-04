701568c81eedd8b3cc7cb70151f9334d3d3ea8ad
holybaechu <66154521+baechooYT@users.noreply.github.com>
2022-09-04 17:20:13 +0900

    a

--- routes/api/verify.js
+++ routes/api/verify.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const ipChecker = require('../../functions/checkIsRobloxIP.js');
+const globalsModule = require('../../globals.js');
 
 router.get('/', async function(req, res) {
+    if (!usernameToGet) { res.status(400).json({ success: false, errors: [{message: 'username could not be parsed from requset.'}]}) } 
+
      // Checking is the real roblox server
     if (ipChecker(req.headers['x-forwarded-for'] || req.socket.remoteAddress) == true){
-        console.log("REAL ROBLOX SERVER !@#$!@#!#@!@!");
-        res.json({success: true, data: "Under development"})
+        if (globalsModule.verifyQueue[usernameToGet]) {
+            globalsModule.verifyQueue[usernameToGet].createDM(true).send("You are verified!")
+            res.json({success: true})
+        }
     }else{
         res.status(400).json({success: false, errors: [{message: "This ip is not seems to be allowed to access this api."}]})
     }
